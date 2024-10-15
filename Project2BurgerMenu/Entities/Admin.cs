29344cdbeeb824e08ef5ee32f2cb6847bef3ef33
GÖZDE SARI <gozdesari88@gmail.com>
2024-10-15 22:43:21 +0300

    Add project files.

---
+++ Project2BurgerMenu/Entities/Admin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project2BurgerMenu.Entities
+{
+    public class Admin
+    {
+        public int AdminId { get; set; }
+        public string Username{ get; set; }
+        public string Password{ get; set; }
+        public string Name{ get; set; }
+        public string Surname{ get; set; }
+        public string Email{ get; set; }
+
+    }
+}
---
+++ Project2BurgerMenu/Migrations/202410101110095_mig2.cs
@@ -0,0 +1,26 @@
+﻿namespace Project2BurgerMenu.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Products", "ProductName", c => c.String(maxLength: 100));
+            AddColumn("dbo.Products", "CategoryId", c => c.Int(nullable: false));
+            CreateIndex("dbo.Products", "CategoryId");
+            AddForeignKey("dbo.Products", "CategoryId", "dbo.Categories", "CategoryID", cascadeDelete: true);
+            DropColumn("dbo.Products", "Name");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Products", "Name", c => c.String(maxLength: 100));
+            DropForeignKey("dbo.Products", "CategoryId", "dbo.Categories");
+            DropIndex("dbo.Products", new[] { "CategoryId" });
+            DropColumn("dbo.Products", "CategoryId");
+            DropColumn("dbo.Products", "ProductName");
+        }
+    }
+}
